37bfb951bfd6f22cf5095f71596d880b416aaf4c
KicherLq <1138122481@qq.com>
2023-10-11 14:10:21 +0800

    destroy bullet which is out of map

--- assets/Scripts/Global/DataManager.ts
+++ assets/Scripts/Global/DataManager.ts
@@ -10,6 +10,8 @@ import { BulletManager } from '../Entity/Bullet/BulletManager';
 
 const ACTOR_SPEED = 100;
 const BULLET_SPEED = 600;
+const MAP_WIDTH = 960;
+const MAP_HEIGHT = 640;
 
 export default class DataManager extends Singleton {
 	static get Instance() {
@@ -72,6 +74,15 @@ export default class DataManager extends Singleton {
             case InputTypeEnum.TimePast: {
                 const {dt} = input;
                 const {bullets} = this.state;
+
+                //倒序遍历子弹用于销毁已经飞出地图外的子弹
+                for(let i = bullets.length - 1; i >= 0; --i) {
+                    const bullet = bullets[i];
+                    if(Math.abs(bullet.position.x) > MAP_WIDTH / 2 || Math.abs(bullet.position.y) > MAP_HEIGHT / 2) {
+                        bullets.splice(i, 1);
+                    }
+                }
+                
                 for (const bullet of bullets) {
                     bullet.position.x += bullet.direction.x * dt * BULLET_SPEED;
                     bullet.position.y += bullet.direction.y * dt * BULLET_SPEED;
